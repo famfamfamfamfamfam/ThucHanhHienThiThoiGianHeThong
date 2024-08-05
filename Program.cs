b61822b8021a9a109b2c7871b929b550e163203d
BinhTran <fam.out16@gmail.com>
2024-08-06 04:50:27 +0700

    Add cac files cua bai thuc hanh hien thi thoi gian voi CS

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace KHOITAOFILECS
+{
+    class HienThiThoiGianCuaHeThong
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine("Datetime Now is: "+localDate);
+        }
+    }
+}
